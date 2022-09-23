ce17da418952c114097aba805b6ce5db9133f030
vebsun95 <veb.sundal@gmail.com>
2022-09-23 14:00:28 +0200

    feat: /catalogue/:id page WIP

--- src/app/catalogue-detailed/pokemon-display/pokemon-display.component.ts
+++ src/app/catalogue-detailed/pokemon-display/pokemon-display.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { PokemonDetailed } from 'src/app/models/pokemon-detailed.model';
 
 @Component({
   selector: 'app-pokemon-display',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon-display.component.css']
 })
 export class PokemonDisplayComponent implements OnInit {
+
+  @Input() pokemon?: PokemonDetailed
 
   constructor() { }
 
