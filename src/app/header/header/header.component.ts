1f94235adf7cbe56b24c687b0b284c141e7632ce
MBanja <MBanja@N-NO-01-04-8521>
2022-09-27 09:18:22 +0200

    fix: moved logout from trainer page to header

--- src/app/header/header/header.component.ts
+++ src/app/header/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Trainer } from 'src/app/models/trainer.model';
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { TrainerService } from 'src/app/services/trainer.service';
 
 
 @Component({
@@ -7,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  constructor() { }
+  //constructor() { }
+
+  //ngOnInit(): void {
+    
+  //}
+
+  private _trainer?: Trainer;
+
+  get trainer() {
+    return this._trainer!;
+  }
+  constructor(private readonly trainerService: TrainerService, private readonly pokemonService: PokemonService) { }
+
+  public logout(){
+    this.trainerService.logout()
+  }
 
   ngOnInit(): void {
-    
+    this._trainer = this.trainerService.trainer
   }
 
 }
